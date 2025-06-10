1aec6ccf620581482622f3c8dc84225f7dbccedb
Avanthika <avavanthika8@gmail.com>
2025-06-10 10:14:02 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         hello world!
         </p>
+         <button onClick={() => setCount(count + 1)}> You clicked {count} times</button>
         <a
           className="App-link"
           href="https://reactjs.org"
